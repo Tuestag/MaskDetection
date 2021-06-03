ee5b96d1358fa7b635bb61ae9218f5846fda0b40
Tuestag <81398090+Tuestag@users.noreply.github.com>
2021-06-03 15:09:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ cnn = load_model(Maskmodel)
 # st.write("Cache Miss for n:",n)
 # return [random.randint(0,1000) for i in range(n)]
 
-@st.cache(ttl=10*5,max_entries=2)
+@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600)
 def app_object_detection():
 
     class OverwritePrediction(VideoProcessorBase):
@@ -112,9 +112,6 @@ def app_object_detection():
     )
 
   
-
-  
-caching.clear_cache()
 
 
 #App
