2b39eb0bba872b6b672afff6fb369c2d4469232b
Tuestag <81398090+Tuestag@users.noreply.github.com>
2021-06-03 12:17:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ cnn = load_model(Maskmodel)
 def hit_news_api(country, n):
  st.write("Cache Miss for n:",n)
  return [random.randint(0,1000) for i in range(n)]
-results = hit_news_api("USA", 5)
-st.write(results)   
+
 def app_object_detection():
 
     class OverwritePrediction(VideoProcessorBase):
