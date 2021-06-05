d4364638cf5822c100edfd26fec9f617b4287d4a
Tuestag <81398090+Tuestag@users.noreply.github.com>
2021-06-05 00:41:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ WEBRTC_CLIENT_SETTINGS = ClientSettings(
 
 cnn = load_model(Maskmodel)
 
-@st.cache(max_entries=10, ttl=3600)
 def app_object_detection():
 
     class OverwritePrediction(VideoProcessorBase):
